39369dc8a505d1558358c0a217fc4852b9cb5e8d
Sabrina <pereira.tsabrina@gmail.com>
2021-03-25 12:49:30 -0300

    practice08-exercise-3-f

---
+++ JS/3/3-f.js
@@ -0,0 +1,5 @@
+/*Unir todos los elementos del array en un único string donde cada mes
+este separado por un guión - (utilizar join).*/
+let month=["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto",
+"Septiembre", "Octubre", "Noviembre", "Diciembre"];
+month.join("-");
