ae93654f43405ae3f14cfc547e00dd581040ad62
Sabrina <pereira.tsabrina@gmail.com>
2021-03-25 12:49:55 -0300

    practice08-exercise-3-g

---
+++ JS/3/3-g.js
@@ -0,0 +1,5 @@
+/*Crear una copia del array de meses que contenga desde Mayo hasta
+Noviembre (utilizar slice).*/
+let month=["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto",
+"Septiembre", "Octubre", "Noviembre", "Diciembre"];
+month.slice(4,11);
