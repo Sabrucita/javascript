c2f612ad82e713ad895519bbc3465c8b9ffa85da
Sabrina <pereira.tsabrina@gmail.com>
2021-03-25 12:46:21 -0300

    practice08-exercise-2-e

---
+++ JS/2/2-e.js
@@ -0,0 +1,5 @@
+/*Crear una variable de tipo string con al menos 10 caracteres y algún
+espacio en blanco. Encontrar la posición del primer espacio en blanco
+y guardarla en una variable (utilizar indexOf).*/
+x="Hello World";
+y=x.indexOf(" ");
