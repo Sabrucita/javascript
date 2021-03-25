859b5dad2f8317431db07ef144e47785b95496e0
Sabrina <pereira.tsabrina@gmail.com>
2021-03-25 12:45:54 -0300

    practice08-exercise-2-d

---
+++ JS/2/2-d.js
@@ -0,0 +1,11 @@
+/*Crear una variable de tipo string con al menos 10 caracteres y generar
+un nuevo string con la primera letra en mayúscula y las demás en minúscula.
+Guardar el resultado en una nueva variable (utilizar substring, toUpperCase,
+toLowerCase y el operador +).*/
+x="Hello World";
+y=x.length;
+firstLetter=x.substring(0,1);
+firstCapitalLetter=firstLetter.toUpperCase();
+restString=x.substring(1,y);
+restStringMinus=restString.toLowerCase();
+result=firstCapitalLetter+restStringMinus;
