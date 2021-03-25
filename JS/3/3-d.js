e970992d0e15a9e7c61d03bec866497e3442aae8
Sabrina <pereira.tsabrina@gmail.com>
2021-03-25 12:48:50 -0300

    practice08-exercise-3-d

---
+++ JS/3/3-d.js
@@ -0,0 +1,5 @@
+/*Quitar un elemento del principio y del final del array (utilizar shift y pop).*/
+let month=["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto",
+"Septiembre", "Octubre", "Noviembre", "Diciembre"];
+month.shift();
+month.pop();
