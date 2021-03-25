f15b2b200927b1231cfde3e0b5e9d48c2f3147f8
Sabrina <pereira.tsabrina@gmail.com>
2021-03-25 12:51:58 -0300

    practice08-exercise-5-a

---
+++ JS/5/5-a.js
@@ -0,0 +1,7 @@
+/*Crear un array que contenga 5 palabras y recorrer dicho array utilizando un bucle
+for de JavaScript para mostrar una alerta utilizando cada una de las palabras.*/
+let days=["Monday","Tuesday","Wensday","Thursday","Friday"];
+for (let index = 0; index < days.length; index++) {
+    const element = days[index];
+		alert(element);
+}
