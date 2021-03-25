213283d0636579da721474d132f0d7867b74472e
Sabrina <pereira.tsabrina@gmail.com>
2021-03-25 12:48:06 -0300

    practice08-exercise-3-c

---
+++ JS/3/3-c.js
@@ -0,0 +1,6 @@
+/*Agregar un elemento al principio y al final del array (utilizar unshift y push).*/
+
+let month=["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto",
+"Septiembre", "Octubre", "Noviembre", "Diciembre"];
+month.unshift("month1","month2");
+month.push("month13","month14");
