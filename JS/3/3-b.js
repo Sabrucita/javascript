cd1fb91b4feb982a6a352c1a68824775050f18f8
Sabrina <pereira.tsabrina@gmail.com>
2021-03-25 12:47:47 -0300

    practice08-exercise-3-b

---
+++ JS/3/3-b.js
@@ -0,0 +1,6 @@
+/*Ordenar el array de meses alfabéticamente y mostrarlo por consola (utilizar sort).
+["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto",
+"Septiembre", "Octubre", "Noviembre", "Diciembre"]*/
+let month=["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto",
+"Septiembre", "Octubre", "Noviembre", "Diciembre"];
+month.sort();
