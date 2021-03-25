43412e010365e60aadcbae59f29ae8a1738909ad
Sabrina <pereira.tsabrina@gmail.com>
2021-03-25 12:49:11 -0300

    practice08-exercise-3-e

---
+++ JS/3/3-e.js
@@ -0,0 +1,4 @@
+/*Invertir el orden del array (utilizar reverse)*/
+let month=["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto",
+"Septiembre", "Octubre", "Noviembre", "Diciembre"];
+month.reverse();
