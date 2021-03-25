3c8442b08a7ee3cd6ee19719bbaf9afa005c1ac6
Sabrina <pereira.tsabrina@gmail.com>
2021-03-25 12:43:39 -0300

    practice08-exercise-1-c

---
+++ JS/1/1-c.js
@@ -0,0 +1,5 @@
+/*Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string)
+ guardando el resultado la suma una 3er variable (utilizar length).*/
+x="Hello ";
+y="World";
+z=x.length+y.length;
